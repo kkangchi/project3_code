225b3c795ff85bba8f5a8128e6b51ae99f12b5bb
kkangchi <pandayy@naver.com>
2026-09-14 16:40:10 +0900

    c

--- server.ts
+++ server.ts
@@ -49,16 +49,17 @@ app.prepare().then(() => {
     handle(req, res);
   });
 
-  // [CRITICAL] Socket.io 및 다른 모든 라우터보다 먼저 req.url을 가로채서 정규화
-  httpServer.prependListener('request', (req, res) => {
-    if (req.url) {
-      if (req.url.startsWith('/socket.io/')) {
-        req.url = '/socket.io' + req.url.slice(10);
-      } else if (req.url === '/socket.io/') {
-        req.url = '/socket.io';
-      }
+// [CRITICAL] Socket.io 및 다른 모든 라우터보다 먼저 req.url을 가로채서 정규화
+httpServer.prependListener('request', (req, _res) => {
+  void _res; // 미사용 변수 경고 방지
+  if (req.url) {
+    if (req.url.startsWith('/socket.io/')) {
+      req.url = '/socket.io' + req.url.slice(10);
+    } else if (req.url === '/socket.io/') {
+      req.url = '/socket.io';
     }
-  });
+  }
+});
 
   // Socket.io 서버 초기화 및 HTTP 서버 바인딩
   initSocketServer(httpServer);
